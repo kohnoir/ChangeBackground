ab8dd8be0f130c4cd36dda92857476fb9b573237
serg0 <serg.01@bk.ru>
2020-01-26 18:45:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/changebackground/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.changebackground;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_PERMISSION_READ_STORAGE = 10;
+    public static final int REQUEST_CODE_PERMISSION_WRITE_STORAGE = 11;
+
+    Button Switch;
+    Button SwitchSecond;
+    Button ok;
+    FrameLayout frameLayout;
+    ImageView view;
+    EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        visible();
+        switchVisible();
+        final int permissionStatus = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+
+                    LoadImg();
+                }
+                else
+                {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_PERMISSION_READ_STORAGE);
+                }
+            }
+        });
+
+    }
+    private void init(){
+        frameLayout = findViewById(R.id.frameLayout);
+        Switch = findViewById(R.id.Switch);
+        SwitchSecond = findViewById(R.id.SwitchSecond);
+        frameLayout.setVisibility(View.VISIBLE);
+        ok = findViewById(R.id.btnOk);
+        editText.findViewById(R.id.edit_text);
+
+    }
+    private void visible(){
+        Switch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                frameLayout.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+    private void switchVisible(){
+        SwitchSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                frameLayout.setVisibility(View.GONE);
+            }
+        });
+    }
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+    private void LoadImg()
+    {
+        String link = editText.getText().toString();
+        view =  findViewById(R.id.imageView);
+        if (isExternalStorageWritable()) {
+
+            File logFile = new File(getApplicationContext().getExternalFilesDir(null),"log.txt");
+            try {
+                FileWriter logWriter = new FileWriter(logFile);
+                logWriter.append("App loaded");
+                logWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                    link);
+
+            Bitmap b = BitmapFactory.decodeFile(file.getAbsolutePath());
+            view.setImageBitmap(b);
+            Toast.makeText(this, file.getAbsolutePath(), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "File Error", Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSION_READ_STORAGE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LoadImg();
+                } else {
+                    // permission denied
+                }
+                return;
+        }
+    }
+}
